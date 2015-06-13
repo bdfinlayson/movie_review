eab39a6f40c6f8b39ba84dd597f3b4d56fae68c2
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-12 20:20:52 -0500

    Add reviews to movies

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,42 +1,27 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_movie
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
-
-  # GET /reviews/new
   def new
     @review = Review.new
   end
 
-  # GET /reviews/1/edit
   def edit
   end
 
-  # POST /reviews
-  # POST /reviews.json
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     if @review.save
-      redirect_to @review
+      redirect_to @movie
     else
       render 'new'
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
@@ -49,8 +34,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
   def destroy
     @review.destroy
     respond_to do |format|
@@ -60,12 +43,14 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+
     def review_params
       params.require(:review).permit(:rating, :comment)
     end
